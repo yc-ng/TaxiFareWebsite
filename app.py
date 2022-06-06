6c82082f38df9e734ed7363e2964f0ca10cdbff5
Yeong Chyuan Ng <yeongchyuan.ng@gmail.com>
2022-06-06 14:09:36 +0800

    Add demo interface in app.py

--- app.py
+++ app.py
@@ -1,47 +1,82 @@
 import streamlit as st
+import requests
+import datetime
 
 '''
-# TaxiFareModel front
+# Taxi fare prediction - demo app
 '''
+url_location = "https://nominatim.openstreetmap.org/search"
+url_predict = "https://taxifare.lewagon.ai/predict"
 
-st.markdown('''
-Remember that there are several ways to output content into your web page...
+# function to
+@st.cache()
+def get_location(location_input):
+    # return a list of an empty dict if no text is provided
+    if location_input == '':
+        return [{}]
 
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
-''')
+    response_pickup = requests.get(url_location,
+                                   params = {'q':location_input,
+                                             'format':'jsonv2'})
+    if response_pickup.status_code == 200:
+        pickup_output = response_pickup.json()
+        return pickup_output
+    # return list of an empty dict if GET request fails
+    else:
+        return [{}]
 
-'''
-## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
+############# Get inputs #################
 
-1. Let's ask for:
-- date and time
-- pickup longitude
-- pickup latitude
-- dropoff longitude
-- dropoff latitude
-- passenger count
-'''
+# get pickup location
+pickup_inp = st.text_input("Pickup Location:", "")
+pickup_out = get_location(pickup_inp)
+st.write(pickup_out[0].get('display_name', ''))
 
-'''
-## Once we have these, let's call our API in order to retrieve a prediction
+# get dropoff location
+dropoff_inp = st.text_input("Dropoff Location:", "")
+dropoff_out = get_location(dropoff_inp)
+st.write(dropoff_out[0].get('display_name', ''))
 
-See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
+# pickup date, time and number of passengers
+columns_dt = st.columns([1, 1, 2])
+# pickup date
+pickup_d = columns_dt[0].date_input("Pickup Date:",
+                            datetime.date.today())
+# pickup time
+pickup_t = columns_dt[1].time_input("Pickup Time:",
+                            datetime.datetime.now())
+# slider for number of passengers
+pax = columns_dt[2].slider('Number of passengers', 1, 8, 1)
+st.write(f'### Picking up {pax} passengers on {pickup_d} {pickup_t}')
 
-🤔 How could we call our API ? Off course... The `requests` package 💡
-'''
+########### Obtain parameters for prediction ##################
 
-url = 'https://taxifare.lewagon.ai/predict'
+# store pickup coordinates
+pickup_lat = pickup_out[0].get('lat', 0)
+pickup_lon = pickup_out[0].get('lon', 0)
+# store pickup coordinates
+dropoff_lat = dropoff_out[0].get('lat', 0)
+dropoff_lon = dropoff_out[0].get('lon', 0)
 
-# if url == 'https://taxifare.lewagon.ai/predict':
+# input params
+params = {
+    "pickup_latitude": pickup_lat,
+    "pickup_longitude": pickup_lon,
+    "dropoff_latitude": dropoff_lat,
+    "dropoff_longitude": dropoff_lon,
+    "passenger_count": pax,
+    "pickup_datetime": f"{pickup_d} {pickup_t}"
+}
 
-#     st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
+############# Obtain prediction #######################
 
-'''
-2. Let's build a dictionary containing the parameters for our API...
+# call prediction API and retrieve the JSON from the response
+if st.button('Get fare estimate'):
+    response_fare = requests.get(url_predict, params=params)
 
-3. Let's call our API using the `requests` package...
-
-4. Let's retrieve the prediction from the **JSON** returned by the API...
-
-## Finally, we can display the prediction to the user
-'''
+    # extract the predicted fare
+    if response_fare.status_code == 200:
+        result_fare = response_fare.json()['fare']
+        st.markdown(f"### Your estimated fare is: **${result_fare:.2f}**")
+    else:
+        st.warning("Something went wrong, please check your inputs and try again.")
